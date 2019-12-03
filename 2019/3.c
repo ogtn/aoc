12841e9cc748e7c05487e8ab21962bc47a7db6ec
Olivier Guittonneau <o.guittonneau@criteo.com>
2019-12-03 19:00:31 +0100

    Day 3, revamp to allow re-read of input

--- 2019/3.c
+++ 2019/3.c
@@ -5,77 +5,91 @@
 
 static const int MAX_SIZE = 32768;
 static const int ORIGIN = MAX_SIZE / 2;
-
 static char map[MAX_SIZE / 4][MAX_SIZE];
 
-int main(void)
+typedef struct segment
 {
-	char buff[8];
+	short len;
 	char direction;
+} segment;
+
+int read(segment *segments)
+{
+	int size = 0;
+	char buff[8];
+
+	do
+	{
+		scanf("%c%[0123456789]s", &segments[size].direction, buff);
+		segments[size++].len = atoi(buff);
+	} while(getchar() == ',');
+
+	return size;
+}
+
+void read_and_mark(segment *segments, int size, int id)
+{
 	int pos_x = ORIGIN;
 	int pos_y = ORIGIN;
 
-	do
+	for(int i = 0; i < size; i++)
 	{
-		scanf("%c%[0123456789]s", &direction, buff);
-		int len = atoi(buff);
-
-		for(int i = 0; i < len; i++)
+		for(int j = 0; j < segments[i].len; j++)
 		{
-			pos_x += (direction == 'R') - (direction == 'L');
-			pos_y += (direction == 'U') - (direction == 'D');
+			pos_x += (segments[i].direction == 'R') - (segments[i].direction == 'L');
+			pos_y += (segments[i].direction == 'U') - (segments[i].direction == 'D');
 
 			if(pos_x < 0 || pos_x >= MAX_SIZE || pos_y < 0 || pos_y >= MAX_SIZE)
 			{
-				printf("Coordinates of cable 1 out of range : [%d,%d]\n", pos_x, pos_y);
-
-				return -1;
+				printf("Coordinates out of range : [%d,%d]\n", pos_x, pos_y);
+				exit(-1);
 			}
 
-			char mask = 1 << ((pos_x % 4) * 2);
+			char mask = 1 << ((pos_x % 4) * 2 + id);
 			map[pos_x / 4][pos_y] |= mask;
 		}
-	} while((direction = getchar()) == ',');
+	}
+}
 
-	pos_x = ORIGIN;
-	pos_y = ORIGIN;
-	getchar();
-
+int find(segment *segments, int size)
+{
 	int best_dst = MAX_SIZE * MAX_SIZE;
+	int pos_x = ORIGIN;
+	int pos_y = ORIGIN;
 
-	do
+	for(int i = 0; i < size; i++)
 	{
-		scanf("%c%[0123456789]s", &direction, buff);
-		int len = atoi(buff);
-
-		for(int i = 0; i < len; i++)
+		for(int j = 0; j < segments[i].len; j++)
 		{
-			pos_x += (direction == 'R') - (direction == 'L');
-			pos_y += (direction == 'U') - (direction == 'D');
+			pos_x += (segments[i].direction == 'R') - (segments[i].direction == 'L');
+			pos_y += (segments[i].direction == 'U') - (segments[i].direction == 'D');
 
-			if(pos_x < 0 || pos_x >= MAX_SIZE || pos_y < 0 || pos_y >= MAX_SIZE)
-			{
-				printf("Coordinates of cable 2 out of range : [%d,%d]\n", pos_x, pos_y);
+			char mask = 0x3 << ((pos_x % 4) * 2);
 
-				return -1;
-			}
-
-			char mask = 1 << ((pos_x % 4) * 2);
-
-			if(map[pos_x / 4][pos_y] & mask)
+			if((map[pos_x / 4][pos_y] & mask) == mask)
 			{
 				int dst = abs((ORIGIN - abs(pos_x))) + abs((ORIGIN - abs(pos_y)));
 
 				if(dst < best_dst)
 					best_dst = dst;
-
-				mask <<= 1;
-				map[pos_x / 4][pos_y] |= mask;
 			}
 		}
-	} while((direction = getchar()) == ',');
+	}
 
-	printf("%d\n", best_dst);
+	return best_dst;
+}
+
+int main(void)
+{
+	segment wire_1[512], wire_2[512];
+	int wire_1_size, wire_2_size;
+
+	wire_1_size = read(wire_1);
+	getchar();
+	wire_2_size = read(wire_2);
+	read_and_mark(wire_1, wire_1_size, 0);
+	read_and_mark(wire_2, wire_2_size, 1);
+	printf("Part 1: %d\n", find(wire_1, wire_1_size));
 
 	return 0;
 }
